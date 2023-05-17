79dee7c57f71ce5b97b916d96ff249cbf1724d9e
Brian Abidin <Brian.abidin1@gmail.com>
2023-05-17 16:00:01 -0400

    added babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
   },
-  mode: "development"
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]]
+          }
+        }
+      }
+    ]
+  }
 };

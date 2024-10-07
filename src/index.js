9a367cde5b162c738dd5cd8db76201b7b54a2c65
Brian Abidin <Brian.abidin1@gmail.com>
2024-10-07 16:46:27 -0400

    added getWeather function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import "./style.css";
 
+async function getWeather() {
+  try {
+    const response = await fetch(
+      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=LHYLQPPB3KWXWZBGLTXZZC7UT",
+      { mode: "cors" }
+    );
+    const weatherData = await response.json();
+    console.log("info", weatherData);
+    console.log("day:", weatherData.days[0].datetime);
+    console.log("temp:", weatherData.days[0].temp);
+    console.log("condition:", weatherData.days[0].conditions);
+    console.log("rain:", weatherData.days[0].precipprob);
+    console.log("wind:", weatherData.days[0].windspeed);
+    // const catData = await response.json();
+    // img.src = catData.data.images.original.url;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// add loop that does getWeather 7 times, one for each day of the week
+// add DOM elements that input this data into the HTML
+
+getWeather();
 console.log("hello");

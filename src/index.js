f99bf9607da0f75d6589bb1883178c56bc69b560
Brian Abidin <Brian.abidin1@gmail.com>
2024-10-07 22:11:42 -0400

    updated addInfoBox function

--- src/index.js
+++ src/index.js
@@ -10,6 +10,33 @@ const condition = document.getElementById("condition-1");
 const rain = document.getElementById("rain-1");
 const wind = document.getElementById("wind-1");
 
+function addInfoBox() {
+  for (let i = 0; i < 7; i += 1) {
+    const infoBox = document.createElement("div");
+    const infoDay = document.createElement("div");
+    const infoTemp = document.createElement("div");
+    const infoCondition = document.createElement("div");
+    const infoRain = document.createElement("div");
+    const infoWind = document.createElement("div");
+
+    infoBox.classList.add("info-box");
+    infoBox.setAttribute("id", `box-${i + 1}`);
+    infoDay.setAttribute("id", `day-${i + 1}`);
+    infoTemp.setAttribute("id", `temp-${i + 1}`);
+    infoCondition.setAttribute("id", `condition-${i + 1}`);
+    infoRain.setAttribute("id", `rain-${i + 1}`);
+    infoWind.setAttribute("id", `wind-${i + 1}`);
+
+    infoBox.appendChild(infoDay);
+    infoBox.appendChild(infoTemp);
+    infoBox.appendChild(infoCondition);
+    infoBox.appendChild(infoRain);
+    infoBox.appendChild(infoWind);
+
+    infoWrapper.appendChild(infoBox);
+  }
+}
+
 async function getWeather(input) {
   try {
     const response = await fetch(
@@ -25,6 +52,7 @@ async function getWeather(input) {
     console.log("wind:", weatherData.days[0].windspeed);
 
     location.textContent = weatherData.resolvedAddress;
+
     day.textContent = weatherData.days[0].datetime;
     temp.textContent = `${weatherData.days[0].temp}F`;
     condition.textContent = weatherData.days[0].conditions;
@@ -40,17 +68,9 @@ searchButton.addEventListener("click", () => {
   getWeather(searchInput.value);
 });
 
-function addInfoBox() {
-  const infoBox = document.createElement("div");
-  for (let i = 0; i < 5; i += 1) {
-    const newDiv = document.createElement("div");
-    infoBox.appendChild(newDiv);
-  }
-  infoWrapper.appendChild(infoBox);
-}
-
-addInfoBox();
-
+window.addEventListener("load", (event) => {
+  addInfoBox();
+});
 // add loop that does getWeather 7 times, one for each day of the week
 // add DOM elements that input this data into the HTML
 
